ac68de3a00a6205904515c8ffb64827625d9a3ac
Yin <Gingkochang@gmail.com>
2022-08-30 16:10:10 +0800

    串接第三方google登入

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,18 +1,21 @@
 class Api::V1::AuthenticationController < ApplicationController
   skip_before_action :authenticate_request
   def login
-    @user = User.find_by_email(params[:email])
-    if  @user&.authenticate(params[:password])
+    if @user = User.find_by_email(params[:email])
       token = jwt_encode(user_id: @user.id)
+      create_token
+    else
+      @user = User.new(username: params[:authentication][:name] ,email:params[:authentication][:email] ,password:params[:authentication][:email])  
+      @user.save
+      create_token
+    end
+  end 
+
+  private
+  def create_token
+    token = jwt_encode(user_id: @user.id)
       render json: {:message => "Ok",
                     :auth_token => token,
                     :user_id => @user.id }, status: :ok
-    else
-      render json: { error: 'unauthorized' }, status: :unauthorized
-    end
-  end
-  def logout
-    @user = User.find_by_email(params[:email])
-    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
-  require 'bcrypt'
-  has_secure_password
-
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
   validates :username, presence: true, uniqueness: true
 
 end
