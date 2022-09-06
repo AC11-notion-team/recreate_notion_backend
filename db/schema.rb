b4c8bcd81bfe441052a262f06c82c1f7f309738c
luna <qqa0705@gmail.com>
2022-09-06 13:31:26 +0800

    合併遠端

--- db/schema.rb
+++ db/schema.rb
@@ -42,16 +42,6 @@ ActiveRecord::Schema.define(version: 2022_09_02_080508) do
     t.string "tail"
   end
 
-  create_table "sharepages", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "page_id"
-    t.string "permission"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["page_id"], name: "index_sharepages_on_page_id"
-    t.index ["user_id"], name: "index_sharepages_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
