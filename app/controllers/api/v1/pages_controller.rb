08f5b23e66302abd85d9b4c0e36ac96db07739bd
Ted <wodf410@gmail.com>
2022-09-10 17:48:25 +0800

    刪除 重複的程式碼

--- app/controllers/api/v1/pages_controller.rb
+++ app/controllers/api/v1/pages_controller.rb
@@ -20,43 +20,32 @@ class Api::V1::PagesController < ApplicationController
   def update
     @page = Page.find(params[:id])
     if @page.update
-<<<<<<< HEAD
-      render json:{message:"page #{@page[:id]} was update"}
-    else 
-      render json:{message:"page #{@page[:id]} couldn't update"} , status:404
-=======
-      render json: { message: "page #{@page.id} was update" }
+      render json: { message: "page #{@page[:id]} was update" }
     else
-      render json: { message: "page #{@page.id} couldn't update" }, status: 404
->>>>>>> feature/connect_frontend_server_login
+      render json: { message: "page #{@page[:id]} couldn't update" }, status: 404
     end
   end
 
   def show
     @page = Page.find(params[:id])
     @blocks = Page.print_all_blocks(@page[:tail])
-    if @page.blocks!=[]
-      @blocks=Page.print_all_blocks(@page[:tail])
-    end  
+    @blocks = Page.print_all_blocks(@page[:tail]) if @page.blocks != []
     if @current_user == @page[:user_id]
-      @state = "is a current user"
+      @state = 'is a current user'
     else
       share = @page[:share]
-      if share 
+      if share
         editable = @page[:editable]
-        if editable
-          @state = "can write"
-        else
-          @state = "can read"
-        end
+        @state = if editable
+                   'can write'
+                 else
+                   'can read'
+                 end
       else
-        @state = "refuse"
+        @state = 'refuse'
       end
     end
-   
   end
-
-
 
   def save_data
     @page = Page.find_by(id: params[:id])
@@ -71,7 +60,7 @@ class Api::V1::PagesController < ApplicationController
     block_data.map.with_index do |block, _index|
       @find_block = Block.where(blockID: block[:id])
       if @find_block.empty?
-        
+
         @block = @page.blocks.new(
           "blockID": block[:id],
           "kind": block[:type],
@@ -98,16 +87,16 @@ class Api::V1::PagesController < ApplicationController
   def share
     @page = Page.find(params[:id])
     prev_share = @page[:share]
-    
-    if @page.update("share" : !prev_share ) 
+
+    if @page.update("share": "#{!prev_share}")
       share = @page[:share]
       if share
-        render json:{"message":"page" : "#{@page[:id]}", "share": "#{@page[:share]}","state": " was update"}
+        render json: { "page": "#{@page[:id]}", "share": "#{@page[:share]}", "state": ' was update' }
       else
-        @page.update("editable" :false)
+        @page.update("editable": 'false')
       end
     else
-      render json:{"message":"page": "#{@page[:id]}","state": "share couldn't update"} , status:404
+      render json: { "page": "#{@page[:id]}", "state": "share couldn't update" }, status: 404
     end
   end
 
@@ -115,13 +104,12 @@ class Api::V1::PagesController < ApplicationController
     @page = Page.find(params[:id])
     prev_editable = @page[:editable]
 
-    if @page.update("editable" : !prev_editable ) 
-      render json:{"message":"share": "#{@page[:share]}","editable": "#{@page[:editable]}","state": "was update"}
+    if @page.update("editable": !prev_editable)
+      render json: { "share": "#{@page[:share]}", "editable": "#{@page[:editable]}", "state": 'was update' }
     else
-      render json:{"message":"page": "#{@page[:id]}","state": "editable couldn't update"} , status:404
+      render json: { "page": "#{@page[:id]}", "state": "editable couldn't update" }, status: 404
     end
   end
-
 
   def delete_data
     @page = Page.find_by(id: params[:page_id])
