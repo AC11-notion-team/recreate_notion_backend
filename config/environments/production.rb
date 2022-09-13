84d0b390f2e44a741ee47857c7145087c250deae
luna <qqa0705@gmail.com>
2022-09-13 18:56:13 +0800

    production rb email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,20 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['ADDRESS'],
+    port: 587,
+    domain: ENV['DOMAIN'],
+    user_name: ENV['USERNAME'],
+    password: ENV['PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "recreate_notion_backend_production"
